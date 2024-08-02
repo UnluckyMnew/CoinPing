5e3fd0309d016d8e09ebd0cb08d2da809a8dc1f8
S. Mnew <yatgrs@gmail.com>
2024-08-02 14:21:54 +0300

    pricing page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,3 +1,26 @@
 export default function Pricing() {
-	return <h1>Pricing 123</h1>
+	return (
+		<section id='pricing' className='pricing__section'>
+			<div className='container'>
+				<div className='pricing__title'>
+					<h1>CoinPing Crypto Data API Plans</h1>
+					<p>
+						From hobbyist to large scale enterprise projects, we’ve got you
+						covered with crypto data sourced by the world’s largest independent
+						crypto data aggregator.
+					</p>
+				</div>
+				<div className="pricing__radio">
+					<div className="radio">
+						<input type="radio" name="plan" id="monthly" />
+						<label htmlFor="monthly">Monthly</label>
+					</div>
+					<div className="radio">
+						<input type="radio" name="plan" id="yearly" />
+						<label htmlFor="yearly">Yearly</label>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
