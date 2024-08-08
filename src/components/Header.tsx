d28b1d14d8c91793b09c3af0d1b7637ce1109f8d
S. Mnew <yatgrs@gmail.com>
2024-08-08 16:06:50 +0300

    header>nav>dynamic fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,48 @@
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+
+interface INavItems {
+	id: number
+	title: string
+	link: string
+	dynamic?: boolean
+}
 
 export const Header = () => {
+	const location = useLocation()
+	const [navItems, setNavItems] = useState<INavItems[]>([
+		{
+			id: 1,
+			title: 'Home',
+			link: '/CoinPing/home',
+			dynamic: true,
+		},
+		{
+			id: 2,
+			title: 'Pricing',
+			link: '/CoinPing/pricing',
+			dynamic: true,
+		},
+		{
+			id: 3,
+			title: 'Market',
+			link: '#market',
+			dynamic: false,
+		},
+		{
+			id: 4,
+			title: 'WhyUs',
+			link: '#whyus',
+			dynamic: false,
+		},
+		{
+			id: 5,
+			title: 'Contact',
+			link: '#footer',
+			dynamic: false,
+		},
+	])
+
 	const activeClick = (): void => {
 		document.querySelector('.nav')?.classList.toggle('active')
 	}
@@ -19,27 +61,22 @@ export const Header = () => {
 
 			<nav className='nav'>
 				<ul>
-					<li>
-						<NavLink to='/CoinPing/home'>Home</NavLink>
-					</li>
-					<li>
-						<NavLink to='/CoinPing/pricing'>Pricing</NavLink>
-					</li>
-					<li>
-						<a className='nav__w' href='#market'>
-							Market
-						</a>
-					</li>
-					<li>
-						<a className='nav__w' href='#whyus'>
-							WhyUs
-						</a>
-					</li>
-					<li>
-						<a className='nav__w' href='#footer'>
-							Contact
-						</a>
-					</li>
+					{navItems.map(item => {
+						if (item.dynamic) {
+							return (
+								<li key={item.id}>
+									<NavLink to={item.link}>{item.title}</NavLink>
+								</li>
+							)
+						}
+						if (location.pathname.split("/").pop() === "home") {
+							return (
+								<li key={item.id}>
+									<a href={item.link}>{item.title}</a>
+								</li>
+							)
+						}
+					})}
 				</ul>
 			</nav>
 		</header>
